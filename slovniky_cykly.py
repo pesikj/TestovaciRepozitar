7ac29c5dbf3d382d3ceb0e051c557fdb8d335afd
Jiří Pešík <jiri.pesik@outlook.cz>
2024-02-14 20:12:34 +0100

    Muj commit

--- slovniky_cykly.py
+++ slovniky_cykly.py
@@ -1,15 +1,7 @@
-product_stock = {
-    'Apple_iPhone 12': 30,
-    'Apple_MacBook Air': 20,
-    'Apple_AirPods Pro': 50,
-    'Samsung_Galaxy S21': 40,
-    'Samsung_Galaxy Tab S7': 25,
+sales = {
+    "Zkus mě chytit": 4165,
+    "Vrah zavolá v deset": 5681,
+    "Zločinný steh": 2565,
 }
-
-def count_vendor_sales(data, vendor):
-    total = 0
-    for key, value in data.items():
-        if vendor in key:
-            total += value
-    return total
-print(count_vendor_sales(product_stock, "Apple"))
+for key, value in sales.items():
+    print(f"Knihy {key} bylo prodáno {value} výtisků.")
